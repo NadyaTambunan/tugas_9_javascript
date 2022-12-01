f071aa0851ef451918d12d15a7da557c77b8f8eb
NadyaTambunan <nadiaputri99tambunan@gmail.com>
2022-12-01 14:35:22 +0700

    tugas9.js

---
+++ tugas9.js
@@ -0,0 +1,14 @@
+function tugas9(){
+    var anak = {
+        nama : "Andi",
+        umur : 10,
+        hobi : "main game",
+        alamat : "medan" 
+    }
+
+    for(var x in anak){
+        console.log(anak[x])
+    }
+}
+
+tugas9()
